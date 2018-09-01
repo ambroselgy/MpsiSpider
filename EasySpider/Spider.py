b87e6698b126ed3053f4e68eec36dc6169cd6970
daijiangtian <413122031@qq@.com>
2018-09-01 18:29:58 +0800

    init

---
+++ EasySpider/Spider.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/1/001 14:17
+# @Author  : Woe
+# @Site    : 
+# @File    : Spider.py
+# @Software: PyCharm
+
+from .Request import Request
+import asyncio
+from EasySpider.utils import get_logger
+
+import multiprocessing
+
---
+++ EasySpider/utils/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/1/001 13:46
+# @Author  : Woe
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+from .logger import get_logger
